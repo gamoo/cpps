911fb36e6906d6cec0d28d6d1ae92570cbc2b4de
Mickye <gamoo12@gmail.com>
2018-02-01 10:46:35 +0900

    ptr,map

---
+++ ConsoleTest/ConsoleTest/maptest.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+#include <map>
+
+void t1()
+{
+	std::cout << "mt::t1()" << std::endl;
+}
---
+++ ConsoleTest/ConsoleTest/ptrtest.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+#include <memory>
+
+void foo()
+{
+	std::cout << "ptrtest::foo()" << std::endl;
+}
