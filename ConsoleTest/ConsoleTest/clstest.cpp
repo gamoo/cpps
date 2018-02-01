f3162d84b9e78cfd3b29ebf485b2febd74af1f64
Mickye <gamoo12@gmail.com>
2018-02-01 11:21:32 +0900

    uniform init

---
+++ ConsoleTest/ConsoleTest/clstest.cpp
@@ -0,0 +1,49 @@
+#include "stdafx.h"
+
+void clst1()
+{
+	class SfxClassName {
+	public:
+		std::string name;
+		std::string description;
+		int index;
+	};
+
+	class SfxClassIndex {
+	public:
+		int index;
+		double value;
+	};
+
+	std::string t = "test";
+
+	SfxClassName scn;
+
+	// uniform initialization
+	SfxClassName scn2 = { "contacts", "read_count", 1 };
+	SfxClassName scn5[] = { { "contacts", "read_count", 1 },{ "contacts", "read_count", 1 } };
+	
+	SfxClassIndex idx { 3, 5.0 };
+	
+	class SfxBasic {
+		// default is private
+	public:
+		int basic;
+	};
+
+	SfxBasic bt;
+
+	bt.basic;
+
+	struct SfxStruct {
+		// default is public
+		int basic;
+	};
+
+	SfxStruct st;
+
+	st.basic;
+
+	
+
+}
--- ConsoleTest/ConsoleTest/ptrtest.cpp
+++ ConsoleTest/ConsoleTest/ptrtest.cpp
@@ -1,7 +1,27 @@
 #include "stdafx.h"
 #include <memory>
+#include <string>
+
+//
+// https://msdn.microsoft.com/ko-kr/library/hh279674.aspx
+// https://msdn.microsoft.com/en-us/library/hh279674.aspx
+// 
 
 void foo()
 {
 	std::cout << "ptrtest::foo()" << std::endl;
+}
+
+// https://msdn.microsoft.com/en-us/library/hh279674.aspx
+// unique_ptr
+
+void unique_test()
+{
+	class Song {
+		std::string title;
+		std::string singer;
+	};
+	
+	std::unique_ptr<Song> song(new Song{ "Nothing", "Test" });
+
 }
